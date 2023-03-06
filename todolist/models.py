47e797648ca4756365f024e20cfd0af27bcf4483
Junus Ergin <junus91@googlemail.com>
2023-03-06 10:30:59 +0100

    08 - Todo-Model anlegen

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+import datetime
 
 # Create your models here.
+class TodoItem(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateField(default=datetime.date.today)
+    checked = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'({self.id})  {self.title}'
